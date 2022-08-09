03de8fda36d5a34ae27bf7c5b646c6565212299e
O-tega <tega.myview@gmail.com>
2022-08-09 12:54:15 +0100

    new changes

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ dotenv.config({path: './config/config.env'})
 
 const app = express()
 
+app.get('/', (req, res)=>{
+    // res.status(200).json({success: true, data: {id: 1}})
+    res.send({hi: 'there'})
+})
+
 PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server running on ${process.env.NODE_ENV} mode on port ${PORT}`))
