5d1cb807e2e2ceab6f1b006734a224a037ec864e
O-tega <tega.myview@gmail.com>
2022-08-19 23:01:37 +0100

    changes to mongo string name

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
 	try{
 		const conn = await mongoose.connect(
-			process.env.MONGO_URI_LOCAL,
+			process.env.MONGO_URI,
 			{
 				// useNewUrlParser: true,
 				// useCreateIndex : true,
