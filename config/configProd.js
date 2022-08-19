f6b06bd4aabc9db1f2d1f215c3e6a57714f6308e
O-tega <tega.myview@gmail.com>
2022-08-19 14:52:35 +0100

    set up production server

---
+++ config/configProd.js
@@ -0,0 +1,10 @@
+//  configProd.js - figure out what set of credentials to return
+
+if(process.env.NODE_ENV ==="production"){
+    //  we are in production - return prod set of keys
+    module.exports = require("./Prod.js")
+}else{
+    //  we are in development - return dev set of keys
+    module.exports = require('./config.env')
+}
+
