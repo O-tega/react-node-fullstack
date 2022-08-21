f956168bf625c011778a6cc1b6278cba74c9ddae
O-tega <tega.myview@gmail.com>
2022-08-21 17:41:05 +0100

    successfully initaize local db

--- config/db.js
+++ config/db.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async ()=>{
-    // try{
+    try{
         const conn=await
-        mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+        mongoose.connect(process.env.MONGO_URI_LOCAL, {
+            // useNewUrlParser: true,
+            // useCreateIndex: true,
+            // useFindAndModify: false
         });
         console.log(`Database is connected successfully at: ${conn.connection.host}`)
-    // }catch{(err)=>{
-    //     console.log('Error: ', err);
-    //     process.exit(1)
-    // }}
-        
+    }catch(err){
+        console.log('Error: ', err);
+        process.exit(1)
+    }
+         
 }
 
 module.exports = connectDB;
