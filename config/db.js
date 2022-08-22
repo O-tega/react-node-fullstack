90ad6095f15f51ba0305537a1eb273b1a89e71ac
O-tega <tega.myview@gmail.com>
2022-08-22 17:57:20 +0100

    completed user model creation

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async ()=>{
     try{
         const conn=await
-        mongoose.connect(process.env.MONGO_URI_LOCAL, {
+        mongoose.connect(process.env.MONGO_URI, {
             // useNewUrlParser: true,
             // useCreateIndex: true,
             // useFindAndModify: false
